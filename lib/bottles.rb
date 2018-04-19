88bc67799275bd27c086dc663b6389f44d3b445a
Siarhei Kisliak <kislak@fidor.com>
2018-04-19 15:55:58 +0300

    final

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,4 +11,12 @@ class Bottles
     template = File.open("lib/bottles_#{t}.txt.erb", 'rb', encoding: 'utf-8', &:read)
     ERB.new(template).result(binding)
   end
+
+  def verses(from, to)
+    from.downto(to).to_a.map { |n| verse(n) }.join("\n")
+  end
+
+  def song
+    verses(99, 0)
+  end
 end
